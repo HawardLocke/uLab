68f784b066fc35e31ccfad3e8009b0dc30ca56b9
HawardLocke <1534921818@qq.com>
2016-10-12 21:10:45 +0800

    Object pool

--- uLab/Assets/Scripts/ResourceManager/ObjectPool.cs
+++ uLab/Assets/Scripts/ResourceManager/ObjectPool.cs
@@ -1,11 +1,147 @@
 
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
 
 namespace Locke
 {
-
-	public class ObjectPool
+	public class ObjectPool : Singleton<ObjectPool>
 	{
 
+		public enum ObjectType
+		{
+			Min = 0,
+
+			Role,
+			Effect,
+			UI,
+
+			Max,
+		}
+
+
+		public class ObjectInfo
+		{
+			public ObjectType objectType;
+		}
+
+
+		public class PoolInfo
+		{
+			// use gameobject as key to prevent the case that the same object be put into pool repeatedly.
+			public Dictionary<GameObject, ObjectInfo> objectsDic = new Dictionary<GameObject, ObjectInfo>();
+		}
+
+
+		// use name of resource as key.
+		private Dictionary<string, PoolInfo> poolInfoDic = new Dictionary<string, PoolInfo>();
+
+		private GameObject poolGameObjectRoot = null;
+
+
+		public GameObject GetGO(string resName)
+		{
+			PoolInfo poolInfo = null;
+			if (!poolInfoDic.TryGetValue(resName, out poolInfo))
+			{
+				poolInfo = new PoolInfo();
+				poolInfoDic.Add(resName, poolInfo);
+			}
+
+			GameObject go = null;
+			ObjectInfo objectInfo = null;
+			foreach(KeyValuePair<GameObject, ObjectInfo> pair in poolInfo.objectsDic)
+			{
+				go = pair.Key;
+				objectInfo = pair.Value;
+				break;
+			}
+
+			if (go == null)
+			{
+				// instantiate one.
+				//go = GameObject.Instantiate();
+			}
+			else
+			{
+				poolInfo.objectsDic.Remove(go);
+				OnGet(go, objectInfo);
+			}
+			
+			return go;
+		}
+
+		public void PutGO(string resName, GameObject go, ObjectType objectType)
+		{
+			if (resName == null || resName == "" || go == null || objectType <= ObjectType.Min || objectType >= ObjectType.Max)
+			{
+				Debug.LogError("failed to call ObjectPool.PutGo: params are invalid.");
+				return;
+			}
+
+			if (poolGameObjectRoot == null)
+			{
+				poolGameObjectRoot = new GameObject("PoolRoot");
+				DontDestroyOnLoad(poolGameObjectRoot);
+			}
+
+			PoolInfo poolInfo = null;
+			if (!poolInfoDic.TryGetValue(resName, out poolInfo))
+			{
+				poolInfo = new PoolInfo();
+				poolInfoDic.Add(resName, poolInfo);
+			}
+
+			ObjectInfo objectInfo = new ObjectInfo();
+			objectInfo.objectType = objectType;
+
+			if (poolInfo.objectsDic.ContainsKey(go))
+			{
+				poolInfo.objectsDic[go] = objectInfo;
+			}
+			else
+			{
+				poolInfo.objectsDic.Add(go, objectInfo);
+			}
+
+			OnPut(go, objectInfo);
+		}
+
+		private void OnGet(GameObject go, ObjectInfo objectInfo)
+		{
+			ObjectType objectType = objectInfo.objectType;
+			switch (objectType)
+			{
+				case ObjectType.Role:
+					break;
+				case ObjectType.Effect:
+					break;
+				case ObjectType.UI:
+					break;
+				default:
+					break;
+			}
+		}
+
+		private void OnPut(GameObject go, ObjectInfo objectInfo)
+		{
+			ObjectType objectType = objectInfo.objectType;
+			switch (objectType)
+			{
+				case ObjectType.Role:
+					break;
+				case ObjectType.Effect:
+					break;
+				case ObjectType.UI:
+					break;
+				default:
+					break;
+			}
+		}
+
 	}
+
 
 }
